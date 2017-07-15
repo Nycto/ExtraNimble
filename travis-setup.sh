dc191ac417f205309a5e5ac3023bbd29361b2ae2
Nycto <James@RoundEights.com>
2017-07-14 19:17:29 -0700

    travis script tweaks

--- travis-setup.sh
+++ travis-setup.sh
@@ -2,6 +2,13 @@
 
 set -e
 set -o pipefail
+set -o xtrace
+
+compile() {
+    ./bin/nim c koch
+    ./koch boot -d:release
+    ./koch nimble
+}
 
 # If Nim and nimble are still cached from the last time
 if [ -x nim/bin/nim ]; then
@@ -9,11 +16,7 @@ if [ -x nim/bin/nim ]; then
     cd nim
     git fetch origin
 
-    if ! git merge FETCH_HEAD | grep "Already up-to-date"; then
-        ./bin/nim c koch
-        ./koch boot -d:release
-        ./koch nimble
-    fi
+    test "$(git rev-parse HEAD)" == "$(git rev-parse @{u})" || compile
 
 # Download nim from scratch and compile it
 else
@@ -21,13 +24,14 @@ else
     git clone -b devel --depth 1 git://github.com/nim-lang/nim nim
     cd nim
 
-    git clone --depth 1 git://github.com/nim-lang/csources csources/
+    git clone --depth 1 git://github.com/nim-lang/csources csources
     (cd csources && sh build.sh)
 
     rm -rf csources
-    bin/nim c koch
-    ./koch boot -d:release
-    ./koch nimble
+
+    compile
 fi
 
 cd ..
+
+ls ./nim/bin
